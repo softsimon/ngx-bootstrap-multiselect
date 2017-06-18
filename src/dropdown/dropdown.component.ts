c0aac6755e7a4e0b2cf99d0aa7c30a7badf0f173
Alex Malkevich <oleksii.malkevych@emaratech.ae>
2017-06-18 16:10:17 +0400

    fix(dropdown): Update title when options changed
    fixes #197

--- src/dropdown/dropdown.component.ts
+++ src/dropdown/dropdown.component.ts
@@ -117,6 +117,7 @@ export class MultiselectDropdown implements OnInit, OnChanges, DoCheck, ControlV
       this.parents = this.options
         .filter(option => typeof option.parentId === 'number')
         .map(option => option.parentId);
+      this.updateTitle();
     }
 
     if (changes['texts'] && !changes['texts'].isFirstChange()) {
