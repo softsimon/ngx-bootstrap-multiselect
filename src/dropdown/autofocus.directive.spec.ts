7fdf7c9850a5ad9228125beda82aa344d295255b
Alex Malkevich <oleksii.malkevych@emaratech.ae>
2017-12-19 18:39:52 +0400

    feat(autofocus): Add autofocus directive For reliable autofocus management

---
+++ src/dropdown/autofocus.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { AutofocusDirective } from './autofocus.directive';
+
+describe('Directive: Autofocus', () => {
+  it('should create an instance', () => {
+    const directive = new AutofocusDirective(<any>{});
+    expect(directive).toBeTruthy();
+  });
+});
